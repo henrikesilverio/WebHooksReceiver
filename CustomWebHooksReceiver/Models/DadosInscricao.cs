d51db8fe85a5c292f6d69a3ef2ac67a50efd8577
henrikesilverio <henrikesilverio@gmail.com>
2016-03-17 13:15:53 -0300

    Inicial

---
+++ CustomWebHooksReceiver/Models/DadosInscricao.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CustomWebHooksReceiver.Models
+{
+    public class DadosInscricao
+    {
+        public string WebHookUri { get; set; }
+        public string Secret { get; set; }
+        public string Description { get; set; }
+        public ISet<string> Filters { get; set; }
+    }
+}
